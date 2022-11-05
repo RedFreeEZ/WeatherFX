228167871bddb7d692d151087fb277940ed3caff
ItsFire143 <112923603+ItsFire143@users.noreply.github.com>
2022-11-05 15:07:40 +0300

    окошечко

--- src/main/java/com/example/weatherfx/Main.java
+++ src/main/java/com/example/weatherfx/Main.java
@@ -7,17 +7,20 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("weatherFrame.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        Weather weather = new Weather("33");
+        weather.MapCityAndTemp();
+        weather.FindCity("Владимир");
+        weather.getWeatherFor8Days();
         launch();
     }
 }
--- src/main/java/com/example/weatherfx/WeatherController.java
+++ src/main/java/com/example/weatherfx/WeatherController.java
@@ -3,7 +3,7 @@ package com.example.weatherfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class WeatherController {
     @FXML
     private Label welcomeText;
 
